c95b8f9fca911cbf919e3f2212e9027c9385d760
martin sellmair <martin.sellmair@usaneers.de>
2024-11-27 11:07:37 +0100

    made all variables and types as explicit as possible

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,11 +2,7 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import test.Model
@@ -14,16 +10,30 @@ import test.Model
 @Composable
 @Preview
 fun App(model: Model) {
-    var testText: String by remember { mutableStateOf("Hello, World!") }
-
-    MaterialTheme {
-        Button(onClick = {
-            model.Increment()
-            testText = "Hello, Desktop!"+model.test
-        }) {
-            Text(testText)
+    var testText: String by remember<MutableState<String>>(
+        calculation = {
+            ->
+            val mutableStateOf: MutableState<String> = mutableStateOf<String>(value = "Hello, World!")
+            return@remember mutableStateOf
         }
-    }
+    )
+
+    MaterialTheme(
+        content = {
+            ->
+            Button(
+                //das ist ein lambda
+                onClick = {
+                    ->
+                    model.Increment()
+                    testText = "Hello, Desktop!" + model.test
+
+                },
+                //wir setzten text auf einen speicherbereich names testText, wenn sich testText ändert, ändert sich auch der dargestellte text automatisch
+                content = { -> Text(text = testText) }
+            )
+        }
+    )
 }
 
 fun main() {
@@ -31,11 +41,14 @@ fun main() {
     val gameThread: Thread = Thread(model)
     gameThread.start()
 
-    application {
-        Window(onCloseRequest = ::exitApplication) {
-            App(model)
-        }
-    }
+    application(
+        content = {
+            ->
+            Window(
+                onCloseRequest = ::exitApplication,
+                content = { -> App(model) }
+            )
+        })
 }
 
 
